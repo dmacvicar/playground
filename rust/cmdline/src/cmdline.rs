f5e4e0562ba0208509b435039a53a6c99f196805
Duncan Mac-Vicar P <dmacvicar@suse.de>
2014-07-10 01:02:39 +0200

    fix build

--- rust/cmdline/src/cmdline.rs
+++ rust/cmdline/src/cmdline.rs
@@ -1,23 +1,24 @@
 
 use std::collections::HashMap;
 use std::str;
+use std::string::String;
 
 fn build() {
-    println!("build!"); 
+    println!("build!");
 }
 
 fn main() {
 
-    let mut cmd_map: HashMap<&str, ||> = HashMap::new();
+    let mut cmd_map: HashMap<&str, fn() -> _> = HashMap::new();
     cmd_map.insert("build", build);
 
     let args = std::os::args().clone();
 
-    for i in args.iter() {
+    for i in args.tail().iter() {
         println!("{}", i);
-        match cmd_map.find(i) {
+        match cmd_map.find_equiv(i) {
             Some(action) => {
-                action();
+                (*action)();
             }
             None => {
                 println!("cmd extern?");
