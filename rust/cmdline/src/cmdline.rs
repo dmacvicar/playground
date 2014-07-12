88e4c0b3f5d08d3b9ecf135332b0c91427da597a
Duncan Mac-Vicar P <dmacvicar@suse.de>
2014-07-12 21:45:38 +0200

    getting close

--- rust/cmdline/src/cmdline.rs
+++ rust/cmdline/src/cmdline.rs
@@ -1,11 +1,37 @@
 
+extern crate getopts;
+
 use std::collections::HashMap;
 use std::str;
 use std::string::String;
+use std::os;
+use getopts::{optopt,optflag,getopts,OptGroup};
 
 fn build() {
     println!("build!");
 }
+
+struct Command {
+
+    /// internal subcommands
+    isubcmds: HashMap<String, fn()>,
+}
+
+impl Command {
+
+    fn new() -> Command {
+        Command {
+            isubcmds: HashMap::new(),
+        }
+    }
+
+    /// adds a subcommand
+    fn add_subcmd(&mut self, name: &str, func: fn()) {
+        self.isubcmds.insert(String::from_str(name), func);
+    }
+
+}
+
 
 fn main() {
 
@@ -14,15 +40,45 @@ fn main() {
 
     let args = std::os::args().clone();
 
-    for i in args.tail().iter() {
-        println!("{}", i);
-        match cmd_map.find_equiv(i) {
+    for cmdname in args.tail().iter() {
+        println!("{}", cmdname);
+        match cmd_map.find_equiv(cmdname) {
             Some(action) => {
                 (*action)();
-            }
+            },
             None => {
+                // external command
+                let extcmd_path: Option<Path> = match os::self_exe_path() {
+                    Some(exe_path) => {
+                        // now we need the name of the current program
+                        let this_prog = args.get(0);
+                        Some(exe_path.join(Path::new(format!("{}-{}", this_prog, cmdname))))
+                    },
+                    None => {
+                        fail!("Impossible to fetch the path of this executable.");
+                        let ret: Option<Path> = None;
+                        ret
+                    },
+                };
+
+                match extcmd_path {
+                    Some(path) => println!("about to execute {}", path.display()),
+                    None => {
+                        fail!("command not known {}", cmdname);
+                    }
+                }
+                //
+
+                //println!("{}", os::self_exe_path());
+                /*
+                let command = format!("{}-{}", cmd);
+                let mut command = match os::self_exe_path() {
+                    Some(path) => Command::new(path.join(command)),
+                    None => Command::new(command),
+                };
                 println!("cmd extern?");
-            }
+                */
+            },
         }
     }
 
